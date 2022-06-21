81e8ba4adfd2ce2ff822d83d54621c4c84a9781a
cocacollllla <cocacollllla@gmail.com>
2022-06-21 13:48:11 +0900

    세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from './pages/Main/Main';
+
+const Routing = () => {
+
+  return (
+    <Router>
+ 
+      <Routes>
+        <Route path={"/"} element={<Main />} />
+      </Routes>
+        
+    </Router>
+  );
+};
+export default Routing;
+
