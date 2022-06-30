a435af2bb78cf24b694a46a8778e098dde9da7f3
fanta-fineapple <das1905@naver.com>
2022-06-30 14:44:26 +0900

    netlify배포, pwa 적용

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,25 @@
 import axios from "axios";
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
 
 const api = axios.create({
-  baseURL: "/ttb/api/"
+  baseURL: `${PROXY}/ttb/api/`
 });
 
 export const bookApi = {
-  info: (isbn) => api.get("ItemLookUp.aspx", { 
-    params: {
-      ttbkey: process.env.REACT_APP_TTB_KEY,
-      itemIdType: 'ISBN13',
-      ItemId: isbn,
-      output: 'js',
-      Version: 20131101
-    }
-  }),
+  info: (isbn) => 
+    api.get("ItemLookUp.aspx", { 
+      params: {
+        ttbkey: process.env.REACT_APP_TTB_KEY,
+        itemIdType: 'ISBN13',
+        ItemId: isbn,
+        output: 'js',
+        Version: 20131101
+      }
+    }),
   search: (term, page) =>
     api.get("ItemSearch.aspx", {
       params: {
-        ttbkey: 'ttbdas19051700001',
+        ttbkey: process.env.REACT_APP_TTB_KEY,
         Query: term,
         QueryType: 'Title',
         MaxResults: 10,
@@ -28,3 +30,4 @@ export const bookApi = {
       }
     })
 };
+
